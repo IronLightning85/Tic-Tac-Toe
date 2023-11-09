087e233a2b47fccc8f784b5e6c2dfe232271a645
CEller03 <coleton.eller@gmail.com>
2023-11-09 11:28:09 -0500

    fixed tile marking

--- index.js
+++ index.js
@@ -7,15 +7,15 @@ var xScore = 0
 var oScore = 0
 
 function turn(id){
-    if(checkWin() == 'empty'){
+    if(checkWin() == ''){
+        console.log("hello")
         if(xTurn == true){
+            console.log(id)
             document.getElementById(id).innerHTML = 'X'
-            document.getElementById(id).tagName ='x'
             xTurn = false
             oTurn = true
         }else{
             document.getElementById(id).innerHTML = 'O'
-            document.getElementById(id).tagName ='o'
             oTurn = false
             xTurn = true
         }
@@ -34,55 +34,49 @@ function turn(id){
 
 function checkWin()
 {
-    winner = "empty";
+    winner = "";
     //check horizontals
-    if(board[0][0].tagName == board[0][1].tagName && board[0][1].tagName == board[0][2].tagName)
+    if(board[0][0].innerHTML == board[0][1].innerHTML && board[0][1].innerHTML == board[0][2].innerHTML)
     {
-        winner = board[0][0].tagName;
+        winner = board[0][0].innerHTML;
     }
-    else if(board[1][0].tagName == board[1][1].tagName && board[1][1].tagName == board[1][2].tagName)
+    else if(board[1][0].innerHTML == board[1][1].innerHTML && board[1][1].innerHTML == board[1][2].innerHTML)
     {
-        winner = board[1][0].tagName;
+        winner = board[1][0].innerHTML;
     }
-    else if (board[2][0].tagName == board[2][1].tagName && board[2][1].tagName == board[2][2].tagName)
+    else if (board[2][0].innerHTML == board[2][1].innerHTML && board[2][1].innerHTML == board[2][2].innerHTML)
     {
-        winner = board[2][0].tagName;
+        winner = board[2][0].innerHTML;
     }
 
     //check veticals
-    else if(board[0][0].tagName == board[1][0].tagName && board[1][0].tagName == board[2][0].tagName)
+    else if(board[0][0].innerHTML == board[1][0].innerHTML && board[1][0].innerHTML == board[2][0].innerHTML)
     {
-        winner = board[0][0].tagName;
+        winner = board[0][0].innerHTML;
     }
-    else if(board[0][1].tagName == board[1][1].tagName && board[1][1].tagName == board[2][1].tagName)
+    else if(board[0][1].innerHTML == board[1][1].innerHTML && board[1][1].innerHTML == board[2][1].innerHTML)
     {
-        winner = board[0][1].tagName;
+        winner = board[0][1].innerHTML;
     }
-    else if (board[0][2].tagName == board[1][2].tagName && board[1][2].tagName == board[2][2].tagName)
+    else if (board[0][2].innerHTML == board[1][2].innerHTML && board[1][2].innerHTML == board[2][2].innerHTML)
     {
-        winner = board[0][2].tagName;
+        winner = board[0][2].innerHTML;
     }
 
     //check horizontals
-    else if(board[0][0].tagName == board[1][1].tagName && board[1][1].tagName == board[2][2].tagName)
+    else if(board[0][0].innerHTML == board[1][1].innerHTML && board[1][1].innerHTML == board[2][2].innerHTML)
     {
-        winner = board[0][0].tagName;
+        winner = board[0][0].innerHTML;
     }
-    else if(board[2][0].tagName == board[1][1].tagName && board[1][1].tagName == board[0][2].tagName)
+    else if(board[2][0].innerHTML == board[1][1].innerHTML && board[1][1].innerHTML == board[0][2].innerHTML)
     {
-        winner = board[2][0].tagName;
+        winner = board[2][0].innerHTML;
     }
 
     return winner;
 }
 
 function reset(){
-    for(var i = 0; i < board.length; i++){
-        for(var j = 0; j < board[i].length; j++){
-            board[i][j].tagName = 'empty'
-       }
-    }
-
     var cells = document.getElementsByClassName("cell")
     for(var k = 0; k < cells.length; k++){
         cells[k].innerHTML = ''
