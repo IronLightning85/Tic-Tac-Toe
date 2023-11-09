2c566875f3df0d8ec70cd1a6ee6f3770299b3b90
IronLightning85 <gochenaurbrennan@gmail.com>
2023-11-09 17:19:42 -0500

    Added green squares. Score still isn't working

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ let board = [[document.getElementById("elem1"), document.getElementById("elem2")
 
 var xTurn = true
 
-var xScore = 0
-var oScore = 0
+let xScore = 0
+let oScore = 0
 
 function turn(id){
     if(checkWin() == ''){
@@ -27,16 +27,24 @@ function turn(id){
     }
 
     
-    if(checkWin() == 'x'){
+    if(checkWin() == 'x')
+    {
         xScore++
+        document.getElementById("playerx").innerHTML = document.getElementById("playerx").innerHTML = "Player X Score: " + xScore;
     }
     else if(checkWin() == 'o')
     {
         oScore++
+        document.getElementById("playero").innerHTML = document.getElementById("playero").innerHTML = "Player O Score: " + oScore;
     }
 
     //call function to check if all squares r full
     //if they are, call reset function
+    if(isFull() && checkWin() != "x" && checkWin() != "0")
+    {
+        alert("Board Full. No Winner. Resetting Board");
+        reset();
+    }
 }
 
 
@@ -47,38 +55,86 @@ function checkWin()
     if(board[0][0].innerHTML == board[0][1].innerHTML && board[0][1].innerHTML == board[0][2].innerHTML)
     {
         winner = board[0][0].innerHTML;
+        if(board[0][0].innerHTML != "")
+        {
+            board[0][0].style.backgroundColor = "green"
+            board[0][1].style.backgroundColor = "green"
+            board[0][2].style.backgroundColor = "green"
+        }
     }
     else if(board[1][0].innerHTML == board[1][1].innerHTML && board[1][1].innerHTML == board[1][2].innerHTML)
     {
         winner = board[1][0].innerHTML;
+        if(board[1][0].innerHTML != "")
+        {
+            board[1][0].style.backgroundColor = "green"
+            board[1][1].style.backgroundColor = "green"
+            board[1][2].style.backgroundColor = "green"
+        }
     }
     else if (board[2][0].innerHTML == board[2][1].innerHTML && board[2][1].innerHTML == board[2][2].innerHTML)
     {
         winner = board[2][0].innerHTML;
+        if(board[2][0].innerHTML != "")
+        {
+            board[2][0].style.backgroundColor = "green"
+            board[2][1].style.backgroundColor = "green"
+            board[2][2].style.backgroundColor = "green"
+        }
     }
 
     //check veticals
     else if(board[0][0].innerHTML == board[1][0].innerHTML && board[1][0].innerHTML == board[2][0].innerHTML)
     {
         winner = board[0][0].innerHTML;
+        if(board[0][0].innerHTML != "")
+        {
+            board[0][0].style.backgroundColor = "green"
+            board[1][0].style.backgroundColor = "green"
+            board[2][0].style.backgroundColor = "green"
+        }
     }
     else if(board[0][1].innerHTML == board[1][1].innerHTML && board[1][1].innerHTML == board[2][1].innerHTML)
     {
         winner = board[0][1].innerHTML;
+        if(board[0][1].innerHTML != "")
+        {
+            board[0][1].style.backgroundColor = "green"
+            board[1][1].style.backgroundColor = "green"
+            board[2][1].style.backgroundColor = "green"
+        }
     }
     else if (board[0][2].innerHTML == board[1][2].innerHTML && board[1][2].innerHTML == board[2][2].innerHTML)
     {
         winner = board[0][2].innerHTML;
+        if(board[0][2].innerHTML != "")
+        {
+            board[0][2].style.backgroundColor = "green"
+            board[1][2].style.backgroundColor = "green"
+            board[2][2].style.backgroundColor = "green"
+        }
     }
 
     //check horizontals
     else if(board[0][0].innerHTML == board[1][1].innerHTML && board[1][1].innerHTML == board[2][2].innerHTML)
     {
         winner = board[0][0].innerHTML;
+        if(board[0][0].innerHTML != "")
+        {
+            board[0][0].style.backgroundColor = "green"
+            board[1][1].style.backgroundColor = "green"
+            board[2][2].style.backgroundColor = "green"
+        }
     }
     else if(board[2][0].innerHTML == board[1][1].innerHTML && board[1][1].innerHTML == board[0][2].innerHTML)
     {
         winner = board[2][0].innerHTML;
+        if(board[2][0].innerHTML != "")
+        {
+            board[2][0].style.backgroundColor = "green"
+            board[1][1].style.backgroundColor = "green"
+            board[0][2].style.backgroundColor = "green"
+        }
     }
 
     return winner;
@@ -89,7 +145,30 @@ function reset(){
     for(var k = 0; k < cells.length; k++){
         cells[k].innerHTML = ''
     }
+
+    //resets background color
+    board[0][0].style.backgroundColor = "#F5F5F5"
+    board[0][1].style.backgroundColor = "#F5F5F5"
+    board[0][2].style.backgroundColor = "#F5F5F5"
+    board[1][0].style.backgroundColor = "#F5F5F5"
+    board[1][1].style.backgroundColor = "#F5F5F5"
+    board[1][2].style.backgroundColor = "#F5F5F5"
+    board[2][0].style.backgroundColor = "#F5F5F5"
+    board[2][1].style.backgroundColor = "#F5F5F5"
+    board[2][2].style.backgroundColor = "#F5F5F5"
 }
 
 
 //implelement function to check if all squares r used
+function isFull()
+{
+    for(let i = 0; i < board.length; i++)
+    {
+        for(let j = 0; j < board[i].length; j++)//iterates throug board. if sn empty square is found, return false. if not. return true after iterations
+            if(board[i][j].innerHTML == "")
+            {
+                return false;
+            }
+    }
+    return true
+}
